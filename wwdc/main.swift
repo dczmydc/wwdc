89f62faebad21108203d5191c31cd2c3ad09f684
Mingyuan Zhong <myzhong11@wireless-s0-so-150-41-136.uchicago.edu>
2017-01-12 10:40:49 -0600

    Initial Commit

---
+++ wwdc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  wwdc
+//
+//  Created by Mingyuan Zhong on 1/12/17.
+//  Copyright © 2017 Mingyuan Zhong. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
